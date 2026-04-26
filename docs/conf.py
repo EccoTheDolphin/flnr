39ebdba506e51d7e669fa41c61991f649313da1c
Anatoly Parshintsev <kupokupokupopo@gmail.com>
2026-04-26 12:26:40 +0300

    adjust documentation build system to be readthedocs-friendly

--- docs/conf.py
+++ docs/conf.py
@@ -7,11 +7,26 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import os
 import sys
+import tomllib
+from pathlib import Path
+
+ROOT = Path(__file__).parents[1]
 
 project = "flnr"
 copyright = "2026, Anatoly Parshintsev"
 author = "Anatoly Parshintsev"
-release = "0.1.0"
+
+with (ROOT / "pyproject.toml").open("rb") as fp:
+    project_data = tomllib.load(fp)
+
+version = project_data["project"]["version"]
+release = version
+
+if (
+    os.environ.get("READTHEDOCS") == "True"
+    and os.environ.get("READTHEDOCS_VERSION_TYPE") != "tag"
+):
+    release = f"{version}+dev"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -47,4 +62,4 @@ python_use_unqualified_type_names = True
 autosummary_generate = True
 nitpicky = False
 
-sys.path.insert(0, os.path.abspath("../src"))
+sys.path.insert(0, str(ROOT / "src"))
