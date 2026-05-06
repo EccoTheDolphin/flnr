38d8fe6c088c6ff36868447a90d1184be2dd022d
Anatoly Parshintsev <kupokupokupopo@gmail.com>
2026-05-06 15:05:14 +0300

    drop None type from _cancel_tasks as it is not needed anymore

--- src/flnr/_async_utils.py
+++ src/flnr/_async_utils.py
@@ -4,13 +4,11 @@ import asyncio
 from typing import Any
 
 
-async def _cancel_tasks(*tasks: asyncio.Task[Any] | None) -> None:
-    live_tasks = [task for task in tasks if task is not None]
-
-    if not live_tasks:
+async def _cancel_tasks(*tasks: asyncio.Task[Any]) -> None:
+    if not tasks:
         return
 
-    for task in live_tasks:
+    for task in tasks:
         task.cancel()
 
-    await asyncio.gather(*live_tasks, return_exceptions=True)
+    await asyncio.gather(*tasks, return_exceptions=True)
