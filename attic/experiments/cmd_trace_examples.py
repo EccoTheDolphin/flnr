3c8e749fde5f23d7ca7580a7fb2efdb2545e7376
Anatoly Parshintsev <kupokupokupopo@gmail.com>
2026-05-11 23:13:39 +0300

    make cmd_trace_examples windows-compatible

--- attic/experiments/cmd_trace_examples.py
+++ attic/experiments/cmd_trace_examples.py
@@ -1,9 +1,14 @@
 import logging
 import os
 import sys
+import tempfile
 from pathlib import Path
 
 import flnr
+
+_MANDATORY_ENV: dict[str, str] = {}
+if sys.platform.startswith("win32"):
+    _MANDATORY_ENV["SYSTEMROOT"] = os.environ["SYSTEMROOT"]
 
 logging.basicConfig(level=logging.INFO)
 
@@ -21,7 +26,7 @@ flnr.run_ex(
     tracer=flnr.CommandTracer.with_selected_environment(
         logger, ["PATH", "LD_LIBRARY_PATH"]
     ),
-    cwd=Path("/tmp"),  # noqa: S108
+    cwd=Path(tempfile.gettempdir()),
 )
 
 print()
@@ -32,7 +37,9 @@ flnr.run_ex(
     | {
         "ENV1": "val1",
         "env2": "val2",
-        "PATH": "/usr/lo ca l/bin:/usr/local:" + os.environ["PATH"],
+        "PATH": os.pathsep.join(
+            ["/usr/lo ca l/bin", "/usr/local", os.environ["PATH"]],
+        ),
     },
     tracer=flnr.CommandTracer.with_changed_environment(logger),
 )
@@ -44,23 +51,38 @@ flnr.run_ex(
     env={
         "ENV1": "val1",
         "env2": "val2",
-        "PATH": "/usr/lo ca l/bin:/usr/local:" + os.environ["PATH"],
-    },
+        "PATH": os.pathsep.join(
+            ["/usr/lo ca l/bin", "/usr/local", os.environ["PATH"]]
+        ),
+    }
+    | _MANDATORY_ENV,
     tracer=flnr.CommandTracer.with_recreated_environment(logger),
+    stdout_monitors=flnr.BIND_TO_PARENT,
+    stderr_monitors=flnr.BIND_TO_PARENT,
 )
 
 print()
 HIDDEN_STYLE_VAR = "FLNR_INTERNAL_COMMAND_TRACE_STYLE"
 logger.info("---secret mode not available via API---")
-os.environ[HIDDEN_STYLE_VAR] = "multiline"
-flnr.run_ex(
-    [sys.executable, PROGRAM_TO_RUN],
-    env=os.environ
-    | {
-        "ENV1": "val1",
-        "env2": "val2",
-        "PATH": "/usr/lo ca l/bin:/usr/local:" + os.environ["PATH"],
-    },
-    tracer=flnr.CommandTracer.with_changed_environment(logger),
-)
-del os.environ[HIDDEN_STYLE_VAR]
+old_value = os.environ.get(HIDDEN_STYLE_VAR)
+try:
+    os.environ[HIDDEN_STYLE_VAR] = "multiline"
+    flnr.run_ex(
+        [sys.executable, PROGRAM_TO_RUN],
+        env=os.environ
+        | {
+            "ENV1": "val1",
+            "env2": "val2",
+            "PATH": os.pathsep.join(
+                ["/usr/lo ca l/bin", "/usr/local", os.environ["PATH"]]
+            ),
+        },
+        tracer=flnr.CommandTracer.with_changed_environment(logger),
+        stdout_monitors=flnr.BIND_TO_PARENT,
+        stderr_monitors=flnr.BIND_TO_PARENT,
+    )
+finally:
+    if old_value is None:
+        os.environ.pop(HIDDEN_STYLE_VAR, None)
+    else:
+        os.environ[HIDDEN_STYLE_VAR] = old_value
